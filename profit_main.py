091df5541fcf7e2b5f7343e1a4d6a683698f6eb2
Live session user <luk036@gmail.com>
2018-03-13 21:02:54 +0800

    use fmt

--- profit_main.py
+++ profit_main.py
@@ -9,11 +9,12 @@ alpha, beta = 0.1, 0.4
 v1, v2 = 10.0, 35.0
 y0 = np.array([0.0, 0.0])  # initial x0
 r = np.array([100.0, 100.0]) # initial ellipsoid (sphere)
+fmt = '{:f} {} {} {}'
 
 E = ell(r, y0)
 P = profit_oracle(p, A, alpha, beta, v1, v2, k)
 yb1, fb, iter, flag, status = cutting_plane_dc(P, E, 0.0, 200, 1e-4)
-print (fb, iter, flag, status)
+print(fmt.format(fb, iter, flag, status))
 
 ui = 1.0
 e1 = 0.003
@@ -23,11 +24,11 @@ e3 = 1.0
 E = ell(r, y0)
 P = profit_rb_oracle(p, A, alpha, beta, v1, v2, k, ui, e1, e2, e3)
 yb1, fb, iter, flag, status = cutting_plane_dc(P, E, 0.0, 200, 1e-4)
-print (fb, iter, flag, status)
+print(fmt.format(fb, iter, flag, status))
 
 E = ell(r, y0)
 P = profit_q_oracle(p, A, alpha, beta, v1, v2, k)
 yb1, fb, iter, flag, status = cutting_plane_q(P, E, 0.0, 200, 1e-4)
-print (fb, iter, flag, status)
+print(fmt.format(fb, iter, flag, status))
 
 
