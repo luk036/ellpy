aef7b4c39b727db5ed38a1fd44da5551b296c6bf
Live session user <luk036@gmail.com>
2018-03-20 15:47:26 +0800

    added code climate badge

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ellpy
 ellipsoid method python code
+[![Maintainability](https://api.codeclimate.com/v1/badges/6ce78bab65047bfe53d6/maintainability)](https://codeclimate.com/github/luk036/ellpy/maintainability)
---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+PgPy
+=====
+"""
+
+from __future__ import print_function
+
+import sys
+import warnings
+
+__all__ = ["chol_ext", "cutting_plane", "ell", "exp1d", "lmi_oracle", "lsq_corr_fn", "neg_cycle", "network_oracle", "outscalings", "profit_main"]
