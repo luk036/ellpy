d29905838ef05177b51aea5393870681ad12b6e8
Danny Luk <luk036@gmail.com>
2018-03-28 22:09:08 +0800

    minor fix

--- oracles/sdp_oracle.py
+++ oracles/sdp_oracle.py
@@ -4,17 +4,17 @@ from .lmi_oracle import *
 
 
 class sdp_oracle:
-    def __init__(self, c, F):
+    def __init__(self, c, F, B):
         self.c = c
-        self.lmi = lmi_oracle(F)
+        self.lmi = lmi_oracle(F, B)
 
     def __call__(self, x, t):
-    f0 = np.dot(self.c, x)
-    fj = f0 - t
-    if fj > 0.0:
-        return self.c, fj, t
+        f0 = np.dot(self.c, x)
+        fj = f0 - t
+        if fj > 0.0:
+            return self.c, fj, t
 
-    g, fj = self.lmi.chk_spd(x)
-    if fj == -1:
-        return g, fj, t
+        g, fj = self.lmi.chk_spd(x)
+        if fj == -1:
+            return g, fj, t
         return self.c, 0.0, f0
