1ebd4db12259e62992037ff3c370daa5fadd6e47
Danny Luk <luk036@gmail.com>
2018-05-16 20:21:42 +0800

    fix the broken build

--- oracles/__init__.py
+++ oracles/__init__.py
@@ -8,4 +8,4 @@ from __future__ import print_function
 import sys
 import warnings
 
-__all__ = ["profit_oracle", "chol_ext", "cholutil", "lmi0_oracle", "lmi_oracle", "lmi_old_oracle", "network_oracle", "neg_cycle", "optscaling_oracle", "qmi_oracle"]
+__all__ = ["profit_oracle", "chol_ext", "lmi0_oracle", "lmi_oracle", "lmi_old_oracle", "network_oracle", "neg_cycle", "optscaling_oracle"]
