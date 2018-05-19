5cf4c8cf3788f423ffdf033a93692c318f74aaf7
Live session user <luk036@gmail.com>
2018-05-19 14:28:03 +0800

    extract variables

--- tests/test_profit.py
+++ tests/test_profit.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import numpy as np
 import time
 from ..oracles.profit_oracle import *
-from ..cutting_plane import *
+from ..cutting_plane import cutting_plane_dc, cutting_plane_q
 from ..ell import ell
 
 
@@ -16,6 +16,7 @@ def benchmark_profit(duration=0.000001):
     """
 
     p, A, k = 20., 40., 30.5
+    params = p, A, k
     alpha, beta = 0.1, 0.4
     v1, v2 = 10., 35.
     a = np.array([alpha, beta])
@@ -25,7 +26,7 @@ def benchmark_profit(duration=0.000001):
     # fmt = '{:f} {} {} {}'
 
     E = ell(r, y0)
-    P = profit_oracle((p, A, k), a, v)
+    P = profit_oracle(params, a, v)
     # yb1, fb, niter, flag, status = cutting_plane_dc(P, E, 0. , 200, 1e-4)
     # print(fmt.format(fb, iter, flag, status))
     _, _, niter, flag, _ = \
@@ -39,7 +40,7 @@ def benchmark_profit(duration=0.000001):
     e3 = 1.
 
     E = ell(r, y0)
-    P = profit_rb_oracle((p, A, k), a, v, (ui, e1, e2, e3))
+    P = profit_rb_oracle(params, a, v, (ui, e1, e2, e3))
     # yb1, fb, niter, flag, status = cutting_plane_dc(P, E, 0. , 200, 1e-4)
     # print(fmt.format(fb, iter, flag, status))
     _, _, niter, flag, _ = cutting_plane_dc(P, E, 0.)
@@ -47,7 +48,7 @@ def benchmark_profit(duration=0.000001):
     assert niter == 42
 
     E = ell(r, y0)
-    P = profit_q_oracle((p, A, k), a, v)
+    P = profit_q_oracle(params, a, v)
     # yb1, fb, niter, flag, status = cutting_plane_q(P, E, 0. , 200, 1e-4)
     # print(fmt.format(fb, iter, flag, status))
     _, _, niter, flag, _ = cutting_plane_q(P, E, 0.)
