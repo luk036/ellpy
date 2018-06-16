725708b1af702f1e20bdfcc9495aa8aaaa062552
Live session user <luk036@gmail.com>
2018-06-16 12:59:23 +0800

    restore license

--- ellpy/release.py
+++ ellpy/release.py
@@ -191,7 +191,7 @@ EllPy is a Python package for the creation, manipulation, and
 study of the structure, dynamics, and functions of projective geometry.
 
 """
-# license = 'BSD'
+license = 'BSD'
 authors = {'luk036': ('Wai-Shing Luk', 'luk036@gmail.com')}
 maintainer = "EllPy Developers"
 maintainer_email = "ellpy-discuss@googlegroups.com"
