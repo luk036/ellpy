d6efa0c684252aa6685aacd14a6267fdcbc9417d
Live session user <luk036@gmail.com>
2018-10-27 09:45:40 +0800

    reduce testing time

--- ellpy/tests/test_corr_fn.py
+++ ellpy/tests/test_corr_fn.py
@@ -6,8 +6,10 @@ from scipy.interpolate import BSpline
 
 
 def test_corr_fn():
-    Y, s = create_2d_isotropic(10, 8)
-    _, num_iters, _ = lsq_corr_bspline2(Y, s, 4)
-    assert num_iters == 123
-    _, num_iters, _ = lsq_corr_poly2(Y, s, 4)
-    assert num_iters >= 629 and num_iters <= 657
+    Y, s = create_2d_isotropic(5, 4)
+    _, num_iters, feasible = lsq_corr_bspline2(Y, s, 4)
+    assert feasible
+    assert num_iters <= 115
+    _, num_iters, feasible = lsq_corr_poly2(Y, s, 4)
+    assert feasible
+    assert num_iters <= 570
--- ellpy/tests/test_mle_corr_fn.py
+++ ellpy/tests/test_mle_corr_fn.py
@@ -7,8 +7,9 @@ from scipy.interpolate import BSpline
 
 
 def test_corr_fn():
-    Y, s = create_2d_isotropic(10, 8, 3000)
-    _, num_iters, _ = mle_corr_bspline(Y, s, 6)
-    assert num_iters == 293
+    Y, s = create_2d_isotropic(5, 4, 3000)
+    _, num_iters, feasible = mle_corr_bspline(Y, s, 4)
+    assert feasible
+    assert num_iters <= 150
     # _, num_iters, _ = mle_corr_poly(Y, s, 6)
     # assert num_iters >= 629 and num_iters <= 657
